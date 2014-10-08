00fb867d88b12a30ee2b35ac214bd854c520fedb
Ryan Sobol <contact@ryansobol.com>
2014-10-07 23:08:37 -0700

    Fix a type with class 2 exercise 1

--- lib/class2/exercise1.rb
+++ lib/class2/exercise1.rb
@@ -1,5 +1,5 @@
 # 5 points
 #
-# Assuming leap-years don't exist, display the hours are in year.
+# Assuming leap-years don't exist, display the hours in a year.
 #
 # TIP: I expect you to use a mathematical equation.
