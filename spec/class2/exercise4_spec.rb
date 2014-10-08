412a8283a9e38cd3b8c4643f414b5fd8dc512ef6
Ryan Sobol <contact@ryansobol.com>
2014-10-08 09:31:35 -0700

    Improve specs for class 2 exercise 4

--- spec/class2/exercise4_spec.rb
+++ spec/class2/exercise4_spec.rb
@@ -4,22 +4,46 @@ RSpec.describe 'Class 2 Exercise 4' do
   end
 
   before do
-    stubs = %w(Samuel Leroy Jackson)
-    allow_any_instance_of(Kernel).to receive(:gets).and_return(*stubs)
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('anything')
   end
 
   it 'outputs to STDOUT' do
     expect { exercise4 }.to output.to_stdout
   end
 
-  it "asks for a person's names and greets them with their full name" do
-    message = <<END
+  context 'with Samuel Leroy Jackson' do
+    before do
+      stubs = %w(Samuel Leroy Jackson)
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(*stubs)
+    end
+
+    it "asks for a person's names and greets them with their full name" do
+      message = <<END
 What's your first name?
 What's your middle name?
 What's your last name?
 Nice to meet you, Samuel Leroy Jackson.
 END
 
-    expect { exercise4 }.to output(message).to_stdout
+      expect { exercise4 }.to output(message).to_stdout
+    end
+  end
+
+  context 'with Melissa Joan Hart' do
+    before do
+      stubs = %w(Melissa Joan Hart)
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(*stubs)
+    end
+
+    it "asks for a person's names and greets them with their full name" do
+      message = <<END
+What's your first name?
+What's your middle name?
+What's your last name?
+Nice to meet you, Melissa Joan Hart.
+END
+
+      expect { exercise4 }.to output(message).to_stdout
+    end
   end
 end
