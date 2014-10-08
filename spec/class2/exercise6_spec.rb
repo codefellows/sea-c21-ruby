69d695c1392ecd2739ede40372dc6c1e588bc173
Ryan Sobol <contact@ryansobol.com>
2014-10-08 09:40:40 -0700

    Improve specs for class 2 exercise 6

--- spec/class2/exercise6_spec.rb
+++ spec/class2/exercise6_spec.rb
@@ -4,19 +4,42 @@ RSpec.describe 'Class 2 Exercise 6' do
   end
 
   before do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('i want a raise')
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('anything')
   end
 
   it 'outputs to STDOUT' do
     expect { exercise6 }.to output.to_stdout
   end
 
-  it 'rudely asks what you want, yells at you, and fires you' do
-    message = <<END
+  context 'i want a raise' do
+    before do
+      stub = 'i want a raise'
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(stub)
+    end
+
+    it 'rudely asks what you want, yells at you, and fires you' do
+      message = <<END
 CAN'T YOU SEE I'M BUSY?! MAKE IT FAST, JOHNSON!
 WHADDAYA MEAN 'I WANT A RAISE'?!? YOU'RE FIRED!!
 END
 
-    expect { exercise6 }.to output(message).to_stdout
+      expect { exercise6 }.to output(message).to_stdout
+    end
+  end
+
+  context 'I want an iPhone' do
+    before do
+      stub = 'I want an iPhone'
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(stub)
+    end
+
+    it 'rudely asks what you want, yells at you, and fires you' do
+      message = <<END
+CAN'T YOU SEE I'M BUSY?! MAKE IT FAST, JOHNSON!
+WHADDAYA MEAN 'I WANT AN IPHONE'?!? YOU'RE FIRED!!
+END
+
+      expect { exercise6 }.to output(message).to_stdout
+    end
   end
 end
