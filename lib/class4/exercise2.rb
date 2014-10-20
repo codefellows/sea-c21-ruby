cda662d96e90d64ccafa1231affa20eb7c6cdb80
Ryan Sobol <contact@ryansobol.com>
2014-10-19 19:41:06 -0700

    Fix alignment of class4 exercise2

--- lib/class4/exercise2.rb
+++ lib/class4/exercise2.rb
@@ -5,12 +5,12 @@
 # Write a program that displays the following table of 90's Hip-Hop hits:
 #
 #   $ ruby exercise2.rb
-#   Can I Kick It?      A Tribe Called Quest     1991
-#   Rump Shaker         Wreckx-n-Effect          1992
-#   Check Yo Self       Ice Cube                 1993
-#   Regulate            Warren G & Nate Dogg     1994
-#   I Got 5 On It       Luniz                    1995
-#   Ready Or Not        The Fugees               1996
+#   Can I Kick It?      A Tribe Called Quest      1991
+#   Rump Shaker         Wreckx-n-Effect           1992
+#   Check Yo Self       Ice Cube                  1993
+#   Regulate            Warren G & Nate Dogg      1994
+#   I Got 5 On It       Luniz                     1995
+#   Ready Or Not        The Fugees                1996
 #
 # TIP #1: Hold all of the information -- song, artist, and year -- in an array.
 #
