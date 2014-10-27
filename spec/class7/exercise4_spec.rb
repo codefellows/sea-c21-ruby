a9923f9496f23bb3fe7763b13862d7d9bcd94bcf
Ryan Sobol <contact@ryansobol.com>
2014-10-27 16:12:51 -0700

    Improve class7 exercises

--- spec/class7/exercise4_spec.rb
+++ spec/class7/exercise4_spec.rb
@@ -4,30 +4,71 @@ RSpec.describe OrangeTree do
   let(:orange_tree) { OrangeTree.new }
 
   describe '#new' do
-    it 'has 50 fruit' do
+    it 'initializes a new OrangeTree to 50 @fruit' do
       expect(OrangeTree.new(50).instance_variable_get(:@fruit)).to eq(50)
     end
 
-    it 'has 20 fruit' do
+    it 'initializes a new OrangeTree to 20 @fruit' do
       expect(OrangeTree.new(20).instance_variable_get(:@fruit)).to eq(20)
     end
 
-    it 'has 50 fruit by default' do
-      expect(orange_tree.instance_variable_get(:@fruit)).to eq(50)
+    it 'initializes a new OrangeTree to 50 @fruit by default' do
+      expect(OrangeTree.new.instance_variable_get(:@fruit)).to eq(50)
+    end
+  end
+
+  describe '#fruit' do
+    it 'returns the value of @fruit' do
+      expect(orange_tree.fruit).to eq(50)
+    end
+  end
+
+  describe '#pick!' do
+    it 'picks 1 fruit and has 49 @fruit remaining' do
+      expect(orange_tree.pick!(1)).to eq(49)
+      expect(orange_tree.fruit).to eq(49)
+    end
+
+    it 'picks 49 fruit and has 1 @fruit remaining' do
+      expect(orange_tree.pick!(49)).to eq(1)
+      expect(orange_tree.fruit).to eq(1)
+    end
+
+    it 'picks 50 fruit and has 0 @fruit remaining' do
+      expect(orange_tree.pick!(50)).to eq(0)
+      expect(orange_tree.fruit).to eq(0)
+    end
+
+    it 'failts to pick 51 fruit and has 50 @fruit remaining' do
+      expect(orange_tree.pick!(51)).to be_nil
+      expect(orange_tree.fruit).to eq(50)
+    end
+
+    it 'picks the default amount of fruit and has 49 @fruit remaining' do
+      expect(orange_tree.pick!).to eq(49)
+      expect(orange_tree.fruit).to eq(49)
+    end
+
+    it 'picks 5 fruit then 25 fruit' do
+      expect(orange_tree.pick!(5)).to eq(45)
+      expect(orange_tree.fruit).to eq(45)
+
+      expect(orange_tree.pick!(25)).to eq(20)
+      expect(orange_tree.fruit).to eq(20)
     end
   end
 
   describe '#==' do
-    it 'equals another orange tree with 20' do
-      expect(orange_tree).to eq(OrangeTree.new(50))
+    it 'equals another instance of OrangeTree with 50 @fruit' do
+      expect(OrangeTree.new).to eq(OrangeTree.new(50))
     end
 
-    it 'does not equals another orange tree with 10' do
-      expect(orange_tree).not_to eq(OrangeTree.new(10))
+    it 'does not equal another instance of OrangeTree with 10 @fruit' do
+      expect(OrangeTree.new).not_to eq(OrangeTree.new(10))
     end
 
-    it 'equals another orange tree with the default amount of fruit' do
-      expect(orange_tree).to eq(OrangeTree.new)
+    it 'equals another default instance of OrangeTree' do
+      expect(OrangeTree.new).to eq(OrangeTree.new)
     end
   end
 end
